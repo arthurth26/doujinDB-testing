1409cbd01bd165d90c9932a992f3aad9176e4faa
Arthur Lee <72230266+arthurth26@users.noreply.github.com>
2025-07-28 14:30:49 +0800

    Update searchContext.tsx

--- app/content/searchContext.tsx
+++ app/content/searchContext.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
 
-interface SearchContext {
+interface SearchContextType {
     searchTerm: string;
     setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
+    category: string;
+    setCategory: React.Dispatch<React.SetStateAction<string>>;
 }
 
-const SearchContext = React.createContext<SearchContext | undefined>(undefined);
+
+const SearchContext = React.createContext<SearchContextType | undefined>(undefined);
+
 
 export const SearchProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [searchTerm, setSearchTerm] = React.useState<string>('');
+    const [category, setCategory] = React.useState<string>('');
 
     return (
-        <SearchContext.Provider value={{ searchTerm, setSearchTerm }}>
-            {children}
+        <SearchContext.Provider value={{ searchTerm, setSearchTerm, category, setCategory }}>
+                {children}
         </SearchContext.Provider>
     );
 }
 
-export function useSearch(): SearchContext {
-    const context = React.useContext(SearchContext);
-    if (!context) {
+export function useSearch(): SearchContextType {
+    const searchcontext = React.useContext(SearchContext);
+
+
+    if (!searchcontext) {
         throw new Error('useSearch must be used within a SearchProvider');
     }
-    return context;
+    return searchcontext;
 }
